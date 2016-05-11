8512a0e6d6b394b4f826df2a40631a99d1cc458a
Allan <chenyongjian@yy.com>
2016-05-11 15:22:52 +0800

    Initial Commit

---
+++ WeChatScan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatScan
+//
+//  Created by it on 5/11/16.
+//  Copyright © 2016 AllanChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
